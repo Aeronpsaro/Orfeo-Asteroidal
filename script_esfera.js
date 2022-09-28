eb5bec0c2371b8f9528b9196cae256bb57717a42
Glitch (glitch-hello-website) <none>
2022-09-28 11:50:39 +0000

    👫👡 Checkpoint ./three.min.js:23279094/551410 ./index.html:23279094/1210 ./script_esfera.js:23279094/3466

---
+++ script_esfera.js
@@ -0,0 +1,45 @@
+/* global THREE */
+
+// Fuentes
+//https://threejs.org/docs/#manual/en/introduction/Creating-a-scene -->
+// https://r105.threejsfundamentals.org/threejs/lessons/threejs-primitives.html  -->
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Objeto esfera (radio, eltos ancho, eltos alto)
+const geometry = new THREE.SphereGeometry(1, 10, 10);
+//ELEGIR UNA OPCIÓN
+//Material con o sin relleno
+      const material = new THREE.MeshBasicMaterial({
+        color: 0xffff00,
+        wireframe: true, //Descomenta para activar modelo de alambres
+      });
+
+const esfera = new THREE.Mesh(geometry, material);
+scene.add(esfera);
+
+//Posición de la cámara
+camera.position.z = 5;
+
+//Bucle de animación
+function animate() {
+  requestAnimationFrame(animate);
+
+  //Modifica rotación de la esfera
+  esfera.rotation.x += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
