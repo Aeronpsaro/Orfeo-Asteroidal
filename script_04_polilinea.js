26d2dd1bf6271794b7f05cf93e78270c416917fe
Glitch (glitch-hello-website) <none>
2022-10-18 14:29:50 +0000

    🚝😜 Checkpoint ./script_04_polilinea.js:23279094/152

--- script_04_polilinea.js
+++ script_04_polilinea.js
@@ -10,8 +10,10 @@ let objetos = [];
 let uniforms = {};
 
 //Perfil
+let grid;
 let plano;
 let perfil;
+let positions;
 // Número de puntos a dibujar, incialmente 0
 let npuntos = 0; 
 
@@ -38,70 +40,70 @@ function init() {
   Esfera(1,0,0,1,10,10, 0xff00ff);
   
  //Rejilla de referencia indicando tamaño y divisiones
-			var grid = new THREE.GridHelper(10, 10);
-			grid.position.set(0, 0, 0);
-			grid.geometry.rotateX( Math.PI / 2 );
-			//Desplaza levemente hacia la cámara
-			grid.position.set(0, 0, 0.05);
+	grid = new THREE.GridHelper(10, 10);
+	grid.position.set(0, 0, 0);
+	grid.geometry.rotateX( Math.PI / 2 );
+	//Desplaza levemente hacia la cámara
+	grid.position.set(0, 0, 0.05);
 			
-			scene.add(grid);
+	scene.add(grid);
 			
-			//Creo un plano en z=0 que no muestro para la intersección
-			let geometryp = new THREE.PlaneGeometry( 20,20 );
-			let materialp = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
-			plano = new THREE.Mesh( geometryp, materialp );
-			//Objs.push(plano);
-			plano.visible = false;
-			scene.add( plano );
+	//Creo un plano en z=0 que no muestro para la intersección
+	let geometryp = new THREE.PlaneGeometry( 20,20 );
+	let materialp = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
+	plano = new THREE.Mesh( geometryp, materialp );
+	//Objs.push(plano);
+	plano.visible = false;
+	scene.add( plano );
 			
-			//Polilínea prealojada ver https://threejs.org/docs/#manual/en/introduction/How-to-update-things
+	//Polilínea prealojada ver https://threejs.org/docs/#manual/en/introduction/How-to-update-things
 			
-			// Polilínea dinámica
-			let geometry = new THREE.BufferGeometry();
-			// Tres valores por punto
-			let positions = new Float32Array( MAX_POINTS * 3 ); 
-			geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+	// Polilínea dinámica
+	let geometry = new THREE.BufferGeometry();
+	// Tres valores por punto
+	positions = new Float32Array( MAX_POINTS * 3 ); 
+	geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
 			
-			//Rango a dibujar			
-			geometry.setDrawRange( 0, npuntos );
-			let material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
-			perfil = new THREE.Line( geometry, material );
-			//Añade al grafo de escena
-			scene.add( perfil );
+	//Rango a dibujar			
+	geometry.setDrawRange( 0, npuntos );
+	let material = new THREE.LineBasicMaterial( { color: 0xff0000 } );
+	perfil = new THREE.Line( geometry, material );
+	//Añade al grafo de escena
+	scene.add( perfil );
   
-    //Activo evento de ratón
-			document.addEventListener( 'mousedown', onDocumentMouseDown );  
+  //Activo evento de ratón
+	document.addEventListener( 'mousedown', onDocumentMouseDown );  
 }
 
 function onDocumentMouseDown( event ) {
-				//Conversión coordenadas del puntero
-				const mouse = {
+	//Conversión coordenadas del puntero
+	const mouse = {
 					x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
 					y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
 				}
 
-				//Define rayo a intersectar
-				raycaster.setFromCamera(mouse, camera)
+	//Define rayo a intersectar
+	raycaster.setFromCamera(mouse, camera)
 			
-				// Busca intersecciones con el plano
-				const intersects = raycaster.intersectObject( plano );
+	// Busca intersecciones con el plano
+	const intersects = raycaster.intersectObject( plano );
 
-				// ¿Hay alguna intersección?
-				if ( intersects.length > 0 ) {
-					//Añade a la pòlilínea
-					const vertices = perfil.geometry.attributes.position.array;
+	// ¿Hay alguna intersección?
+	if ( intersects.length > 0 ) {
+		//Añade a la pòlilínea
+		const vertices = perfil.geometry.attributes.position.array;
 					
-					vertices[ npuntos*3 ] = intersects[0].point.x;
-					vertices[ npuntos*3+1 ] = intersects[0].point.y;
-					vertices[ npuntos*3+2 ] = intersects[0].point.z;
-					npuntos++;
+		vertices[ npuntos*3 ] = intersects[0].point.x;
+		vertices[ npuntos*3+1 ] = intersects[0].point.y;
+		vertices[ npuntos*3+2 ] = intersects[0].point.z;
+		npuntos++;
 					
-					console.log(intersects[0].point)
+		console.log(intersects[0].point)
 					
-          //Dibuja esfera
-					Esfera(intersects[0].point.x,intersects[0].point.y, 0,0.2,0xff00ff)
-				}
-			}
+    //Dibuja esfera
+		Esfera(intersects[0].point.x,intersects[0].point.y, 0,0.2,0xff00ff)
+	  }
+	}
 
 function Esfera(px,py,pz, radio, nx, ny, col) {
   let geometry = new THREE.SphereBufferGeometry(radio, nx, ny)
