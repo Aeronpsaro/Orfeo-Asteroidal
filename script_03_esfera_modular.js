5d650c2a52abdeb6c689d6f7e3b9c0282a3089a7
Glitch (glitch-hello-website) <none>
2022-10-19 10:39:26 +0000

    🏌🛥 Checkpoint ./script_03_esfera_modular.js:23279094/890 ./index.html:23279094/217

--- script_03_esfera_modular.js
+++ script_03_esfera_modular.js
@@ -12,9 +12,23 @@ init()
 animationLoop()
 
 function init() {
+  
+  var info = document.createElement('div');
+			info.style.position = 'absolute';
+			info.style.top = '30px';
+			info.style.width = '100%';
+			info.style.textAlign = 'center';
+			info.style.color = '#fff';
+			info.style.fontWeight = 'bold';
+			info.style.backgroundColor = 'transparent';
+			info.style.zIndex = '1';
+			info.style.fontFamily = 'Monospace';
+			info.innerHTML = "three.js - mi texto aquí";
+			document.body.appendChild(info);
+  
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set(0, 0, 50);
+  camera.position.set(0, 0, 10);
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -23,6 +37,21 @@ function init() {
   //Objetos
   Esfera(0,0,0,1,10,10, 0xffff00);
   Esfera(1,0,0,1,10,10, 0xff00ff);
+  
+  var controls = new THREE.OrbitControls(camera, renderer.domElement);
+  
+  //Rejilla de referencia indicando tamaño y divisiones
+var grid = new THREE.GridHelper(20, 40);
+//MOstrarla en vertical
+grid.geometry.rotateX( Math.PI / 2 );
+grid.position.set(0, 0, .05);
+scene.add(grid);
+  
+  //Manejador de evento del botón
+document.getElementById("onoff").addEventListener("click",
+			function(){
+				grid.visible = !grid.visible;
+			});
   
 }
 
