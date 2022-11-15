19f9c38009ced28e3cb8c2899b28425dd72749b8
Glitch (glitch-hello-website) <none>
2022-11-15 22:51:59 +0000

    Primera versión del Sistema Orfeo tras la catástrofe

--- README.md
+++ README.md
@@ -1,43 +1,21 @@
-# Sistema estelar Orfeo
+# Sistema estelar Orfeo 2: Electric Boogaloo
 
-![Muestra.png](Muestra.png)
+
+![MuestraAsteroidal.png](MuestraAsteroidal.png)
 
 ## Introducción
 
-Este sistema estelar, bautizado como Orfeo, implementa de manera general lo visto en las
-lecciones de visión por computador con las siguientes adiciones:
+Orfeo ha sido golpeado por una catástrofe de dimensiones cataclísmicas.
+Un enorme meteoro ha colisionado con la estrella y sus fragmentos, ahora
+asteroides, orbitan atravesando los planetas exteriores.
+¿Se recuperará Orfeo de este descalabro?
 
-## Generación procedural
-Valores aleatorios para el radio, la posición y demás propiedades de los planetas
-se han usado para generar variedad, teniendo en cuenta que cada planeta
-estará más lejos de la estrella del sistema que el anterior.  
-El [generador de texturas de planetas de Rye Terrell](http://wwwtyro.github.io/procedural.js/planet1/),
-sirvió para dar vida a la superficie y atmósfera de los planetas.
-Se tuvo especial cuidado con entre otros el parámetro de la altura de los océanos para
-asegurar que todos los planetas fueran interesantes y únicos.  
-El generador provee de un mapa normal pero no de un mapa de deformaciones locales
-para el planeta, así que con el ánimo de usar este tipo de mapa visto en la asignatura
-se trataron estos mapas normales en el editor gráfico GIMP mediante un filtro
-de Sobel en color y una posterior conversión a mapa de grises.  
-Para simplificar el uso de texturas el parámetro que se pasa en la creación
-de los planetas y las lunas es el índice, con lo que para añadir un
-nuevo planeta bastaría con crear las texturas, guardarlas en la carpeta "Pi" (siendo i
-el índice del íesimo planeta añadido) e incrementar en uno el número de planetas a
-generar en el bucle.
-
-## Visión otrográfica
-Con el objetivo de hacer al segundo punto de vista más útil, se superpuso en la esquina
-superior derecha del principal y se usó una cámara ortográfica para realizar una
-proyección de la planta del sistema que también se actualizara en tiempo real,
-para facilitar mantener la orientación durante el desplazamiento a través del sistema.
-
-## Anillos, nubes, rotación e inclinación
-Para dotar de mayor dinamismo y realismo al sistema se añadión una velocidad de giro
-sobre su propio eje (que además puede estar inclinado con respecto al ecuador de la
-estrella), del mismo modo que su satélite puede estarlo.  
-Además, las nubes de los planetas (y lunas) giran con una velocidad de giro adicional
-a la propia del planeta.  
-Incluso se ha añadido un planeta con anillos y rotación a la propia estrella de Orfeo.  
-
-### Notas
-- Emplear las flechas de dirección para desplazar la cámara.
+## Campo de asteroides
+El campo de asteroides se ha creado usando un umbral sobre el ruido de Perlin por
+gradiente. Además, la zona externa a los «asteroides» se ha marcado como transparente.
+Además se han utilizado las fronteras de un diagrama de Voronoi generado automáticamente
+(fuertemente inspirado en uno de los ejemplos del Libro de los Sombreadores), generando
+unas «rajas» que pretenden simular fracturas en los asteroides para que se parezcan más
+a rocas que a manchas. También se les ha añadido una velocidad angular para que orbiten
+a un ritmo distinto del resto de planetas. La constante SCALE del sombreador de fragmento
+puede usarse para alterar el número de asteroides y su tamaño.
--- SistemaEstelar.js
+++ SistemaEstelar.js
@@ -11,6 +11,7 @@ let t0 = 0;
 let accglobal = 0.001;
 let timestamp;
 let starLight;
+let uniforms;
 
 init()
 animationLoop()
@@ -75,11 +76,142 @@ function init() {
 	renderer2.render( scene, camera2 );
 	
 	//Rejilla de referencia indicando tamaño y divisiones
-	grid = new THREE.GridHelper(20, 40);
-	//MOstrarla en vertical
-	grid.geometry.rotateX( Math.PI / 2 );
+	let gridGeom = new THREE.PlaneGeometry(30, 30);
+	function vertexShader() {return `
+        varying vec2 pos; 
+
+        void main() {
+            pos = uv; 
+
+            vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+            gl_Position = projectionMatrix * modelViewPosition; 
+        }
+    `;}
+	function fragmentShader() {return `
+		#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+varying vec2 pos;
+
+
+// Valor aleatorio en 2D
+vec2 random2(vec2 st){
+  st = vec2( dot(st,vec2(127.1,311.7)),
+            dot(st,vec2(269.5,183.3)) );
+  return -1.0 + 2.0*fract(sin(st)*43758.5453123);
+}
+
+mat2 rotate2d(float _angle){
+    return mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle));
+}
+
+// Gradient Noise by Inigo Quilez - iq/2013
+// https://www.shadertoy.com/view/XdXGW8
+float noise(vec2 st) {
+  vec2 i = floor(st);
+  vec2 f = fract(st);
+
+  // Interpolación con Hemite cúbico
+  //vec2 u = smoothstep(0.,1.,f); // Equivalente
+  vec2 u = f*f*(3.0-2.0*f);
+
+  return mix( mix( dot( random2(i + vec2(0.0,0.0) ), f - vec2(0.0,0.0) ),
+                   dot( random2(i + vec2(1.0,0.0) ), f - vec2(1.0,0.0) ), u.x),
+              mix( dot( random2(i + vec2(0.0,1.0) ), f - vec2(0.0,1.0) ),
+                   dot( random2(i + vec2(1.0,1.0) ), f - vec2(1.0,1.0) ), u.x), u.y);
+}
+
+// Permutation polynomial: (34x^2 + x) mod 289
+vec4 permute(vec4 x) {
+  return mod((34.0 * x + 1.0) * x, 289.0);
+}
+
+// Cellular noise, returning F1 and F2 in a vec2.
+// Speeded up by using 2x2 search window instead of 3x3,
+// at the expense of some strong pattern artifacts.
+// F2 is often wrong and has sharp discontinuities.
+// If you need a smooth F2, use the slower 3x3 version.
+// F1 is sometimes wrong, too, but OK for most purposes.
+vec2 cellular2x2(vec2 P) {
+#define K 0.142857142857 // 1/7
+#define K2 0.0714285714285 // K/2
+#define jitter 0.8 // jitter 1.0 makes F1 wrong more often
+vec2 Pi = mod(floor(P), 289.0);
+vec2 Pf = fract(P);
+vec4 Pfx = Pf.x + vec4(-0.5, -1.5, -0.5, -1.5);
+vec4 Pfy = Pf.y + vec4(-0.5, -0.5, -1.5, -1.5);
+vec4 p = permute(Pi.x + vec4(0.0, 1.0, 0.0, 1.0));
+p = permute(p + Pi.y + vec4(0.0, 0.0, 1.0, 1.0));
+vec4 ox = mod(p, 7.0)*K+K2;
+vec4 oy = mod(floor(p*K),7.0)*K+K2;
+vec4 dx = Pfx + jitter*ox;
+vec4 dy = Pfy + jitter*oy;
+vec4 d = dx * dx + dy * dy; // d11, d12, d21 and d22, squared
+// Sort out the two smallest distances
+#if 0
+// Cheat and pick only F1
+d.xy = min(d.xy, d.zw);
+d.x = min(d.x, d.y);
+return d.xx; // F1 duplicated, F2 not computed
+#else
+// Do it right and find both F1 and F2
+d.xy = (d.x < d.y) ? d.xy : d.yx; // Swap if smaller
+d.xz = (d.x < d.z) ? d.xz : d.zx;
+d.xw = (d.x < d.w) ? d.xw : d.wx;
+d.y = min(d.y, d.z);
+d.y = min(d.y, d.w);
+return sqrt(d.xy);
+#endif
+}
+
+#define SCALE 15.
+
+void main() {
+    //vec2 st = gl_Position.xy;//gl_FragCoord.xy/u_resolution.xy;
+    vec2 st = pos;
+    st -= 0.5;
+    st *= SCALE;
+    st = rotate2d(u_time)*st;
+    vec3 color = vec3(0.0);
+
+    // Escalado
+    vec2 pos = vec2(st*5.0);
+    // Función de ruido
+    float noise = noise(pos);
+    
+    vec2 F = cellular2x2(st*20.);
+
+pos = st-.5;
+float a = 100000.;//-u_time*0.1;
+
+float n = 1.-step(abs(sin(a))-.1,.05+(F.x-F.y)*2.) ;
+    
+    color = vec3( noise*.5+.5 )*step(0.2,noise)*smoothstep(0.1*SCALE,0.15*SCALE,distance(vec2(0.),st)*0.5)*smoothstep(0.25*SCALE,0.2*SCALE,distance(vec2(0.),st)*0.5)*n;
+
+    gl_FragColor = vec4(color, step(0.2,noise)*step(0.1,smoothstep(0.1*SCALE,0.15*SCALE,distance(vec2(0.),st)*0.5)*smoothstep(0.25*SCALE,0.2*SCALE,distance(vec2(0.),st)*0.5)));
+    }
+	  `;}
+	uniforms = {
+		u_time: { value: 0 },
+		u_resolution:  { value: new THREE.Vector2() },
+	};
+	let gridMat = new THREE.ShaderMaterial({
+		vertexShader: vertexShader(),
+		fragmentShader: fragmentShader(),
+		uniforms: uniforms,
+		transparent: true,
+		side: THREE.DoubleSide
+	});
+	//const gridMat = new THREE.MeshBasicMaterial({color:0xffffff, side: THREE.DoubleSide});
+	grid = new THREE.Mesh(gridGeom, gridMat);
+	//Mostrarla en vertical
+	//grid.geometry.rotateX( Math.PI / 2 );
 	grid.position.set(0, 0, .05);
-	//scene.add(grid);
+	scene.add(grid);
 
 	starLight = new THREE.PointLight(0xffffff,2,0,2);
 	scene.add(starLight);
@@ -295,8 +427,12 @@ function creaLuna(planeta, radio, dist, vel, rotate, col, f1, f2, i=undefined) {
 		t1 = new Date();
 		let secs = (t1 - t0) / 1000;
 		flyControls.update(0.5 * secs);
+
+		uniforms.u_resolution.value.set(renderer.domElement.width, renderer.domElement.height);
+		uniforms.u_time.value = timestamp;
 		
 		renderer.render( scene, camera );
+		uniforms.u_resolution.value.set(renderer2.domElement.width, renderer2.domElement.height);
 		renderer2.render( scene, camera2 );
 		
 	}
